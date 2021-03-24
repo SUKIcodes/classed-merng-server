5b6703974892db503f9d1d20042d213c41824d2f
KING <irealking2000@gmail.com>
2021-03-24 21:12:45 +0530

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB:
+      'mongodb+srv://classed:L7pHAwWRqj1bKw9r@cluster0.cifsj.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+  };
