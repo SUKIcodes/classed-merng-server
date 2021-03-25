0db78cf8749b4738b72fc644ab7698b2dd749fa0
SUKIcodes <75484389+SUKIcodes@users.noreply.github.com>
2021-03-25 11:24:18 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGODB:
-      'mongodb+srv://classed:L7pHAwWRqj1bKw9r@cluster0.cifsj.mongodb.net/merng?retryWrites=true&w=majority',
-    SECRET_KEY: 'some very secret key'
-  };
